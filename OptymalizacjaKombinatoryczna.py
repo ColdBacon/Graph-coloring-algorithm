290e321237fc9ee114826fcb76013659dc9c2973
JoachimMakowski <joachim.makowski@student.put.poznan.pl>
2019-11-09 22:27:47 +0100

    Zrobione posortowane wierzchołki według stopnia

--- OptymalizacjaKombinatoryczna.py
+++ OptymalizacjaKombinatoryczna.py
@@ -1,8 +1,11 @@
-
+#!/usr/bin/python3
 txt=[]      #dane wejściowe z pliku
 tablica=[]  #zmiana na int wyjsciowego pliku
 slownik={}  #wierzcholek:kolor
+slownik_2={}
 n=0         #ilosc wierzcholkow
+lista_posortowanych=[]
+
 
 with open('krawedzie.txt') as plik:
     for linia in plik:
@@ -14,12 +17,12 @@ with open('krawedzie.txt') as plik:
 print(n)
 for i in range(n):
     slownik[i+1]=-1
+    slownik_2[i+1]=-1
 
 for [x,y] in txt:
     y=int(y)
     x=int(x)
     tablica.append([x,y])
-print(tablica)
 
 kolor=1             #numery oznaczają kolory
 lista_wychodz=[]    #lista z wierzcholkami polaczonymi z jednym wierzcholkiem do którego dobieramy kolor
@@ -36,7 +39,6 @@ for i in range(1,n+1):
     slownik[i]=kolor
     lista_wychodz=[]
     kolor=1
-print(slownik)
 
 maxi=0
 for x in slownik.values():
@@ -52,7 +54,7 @@ for [x,y] in txt:
     y=int(y)
     x=int(x)
     TABLICA.append([x,y])
-print(TABLICA)
+print("Tablica:",TABLICA)
 
 stopnie_wierzcholkow = {}
 for x in range(1,n+1):
@@ -62,8 +64,28 @@ for [x,y] in tablica:
     stopnie_wierzcholkow[x]+=1
     stopnie_wierzcholkow[y]+=1
 
-print (stopnie_wierzcholkow)
+print ("stopnie:",stopnie_wierzcholkow)
 
 posortowane_wierzcholki = sorted(stopnie_wierzcholkow.items(), key=lambda x: x[1], reverse=True)
-print(posortowane_wierzcholki)
+print("posortowane:",posortowane_wierzcholki)
 
+for x in posortowane_wierzcholki:
+    lista_posortowanych.append(x[0])
+
+print(lista_posortowanych)
+
+for i in lista_posortowanych:
+    for [x,y] in tablica:
+        if x==i:
+            lista_wychodz.append(y)
+        elif y==i:
+            lista_wychodz.append(x)
+    print(i,lista_wychodz)
+    for li in lista_wychodz:
+        if slownik[li]==kolor:
+            kolor+=1
+    slownik_2[i]=kolor
+    lista_wychodz=[]
+    kolor=1
+
+print(slownik_2)
