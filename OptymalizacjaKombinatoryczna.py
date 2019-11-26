a6aee44acdc8da583e62809941adc9f2ae3d3f2b
JoachimMakowski <joachim.makowski@student.put.poznan.pl>
2019-11-26 15:41:16 +0100

    porzadek

--- OptymalizacjaKombinatoryczna.py
+++ OptymalizacjaKombinatoryczna.py
@@ -7,15 +7,16 @@ slownik={}  #wierzcholek:kolor
 slownik_2={}
 n=0         #ilosc wierzcholkow
 lista_posortowanych=[]
+plik_z_krawedziami='graff0.3.txt'
 
-def max_slownik(slowniki):
+def max_slownik(slowniki):      #zwraca ilosc kolorow
     maxi=0
     for x in slowniki.values():
         if x>maxi:
             maxi=x
     return maxi
 
-def kolorowanie(macierz_polaczen,lista_krawedzi):
+def kolorowanie(macierz_polaczen,lista_krawedzi):       #funkcja kolorujaca zachlannie i zwracajaca slownik {indeks_wierzcholka:numer_koloru}
     lista_wychodzacych=[]   #lista z wierzcholkami polaczonymi z jednym wierzcholkiem do którego dobieramy kolor
     slownik_kolorow={}
     for i in range(1,len(lista_krawedzi)+1):
@@ -35,20 +36,24 @@ def kolorowanie(macierz_polaczen,lista_krawedzi):
         kolor=1
     return slownik_kolorow
 
-def losowanie_liczb(n):
+def losowanie_liczb(n):                         #zwraca liste z losowa kolejnoscia wierzcholkow
     lista_losowa=[i for i in range(1,n+1)]
     permutacja=list(permutations(lista_losowa))
     a=random.randint(0,len(permutacja))
     return permutacja[a]
 
-with open('graff0.3.txt') as plik:
+def lista_kolorow(slownik_kolorow):             #zwraca tablice kolorow
+    a=[i for i in slownik_kolorow.values()]
+    return a
+
+with open(plik_z_krawedziami) as plik:
     for linia in plik:
         if(len(linia.strip().split())>1):
             txt.append(linia.strip().split())
         else:
             for x in linia.strip().split():
                 n=int(x)
-print(n)
+
 for i in range(n):
     slownik[i+1]=-1
     slownik_2[i+1]=-1
@@ -58,23 +63,11 @@ for [x,y] in txt:
     x=int(x)
     tablica.append([x,y])
 
-kolor=1             #numery oznaczają kolory
-lista_wychodz=[]    #lista z wierzcholkami polaczonymi z jednym wierzcholkiem do którego dobieramy kolor
-for i in range(1,n+1):
-    for [x,y] in tablica:
-        if x==i:
-            lista_wychodz.append(y)
-        elif y==i:
-            lista_wychodz.append(x)
-    for li in lista_wychodz:
-        if slownik[li]==kolor:
-            kolor+=1
-    slownik[i]=kolor
-    lista_wychodz=[]
-    kolor=1
-
+lista_1_do_n=[i for i in range(1,n+1)]
+slownik = kolorowanie(tablica,lista_1_do_n)
 print("Ilość kolorow:",max_slownik(slownik))
 
+#zachlanny ulepszony
 print(40*"-")
 stopnie_wierzcholkow = {}
 for x in range(1,n+1):
@@ -84,35 +77,31 @@ for [x,y] in tablica:
     stopnie_wierzcholkow[x]+=1
     stopnie_wierzcholkow[y]+=1
 
-posortowane_wierzcholki = sorted(stopnie_wierzcholkow.items(), key=lambda x: x[1], reverse=True)
+posortowane_wierzcholki = sorted(stopnie_wierzcholkow.items(), key=lambda x: x[1], reverse=True) #wierzcholki posortowane po stopniu
 
 for x in posortowane_wierzcholki:
     lista_posortowanych.append(x[0])
-
-for i in lista_posortowanych:
-    for [x,y] in tablica:
-        if x==i:
-            lista_wychodz.append(y)
-        elif y==i:
-            lista_wychodz.append(x)
-    for li in lista_wychodz:
-        if slownik_2[li]==kolor:
-            kolor+=1
-    slownik_2[i]=kolor
-    lista_wychodz=[]
-    kolor=1
+slownik_2=kolorowanie(tablica,lista_posortowanych)
 
 liczba_kolorow = max_slownik(slownik_2)
 print("Ilosc kolorow: ",liczba_kolorow)
 
+
+
+
+
+
 M = [[0 for i in range(n)] for j in range(n)] #tabu matrix
 slownik_LF=kolorowanie(tablica,lista_posortowanych)
-print(max_slownik(slownik_LF))
+
+print(lista_kolorow(slownik_2))
 
 NB = 0
 s = slownik_2 #initial confugiration generated with a greedy algorithm
 NC = liczba_kolorow - 1
 MAX  = 10 #limit of the iterations
+
+#deklarowanie zmiennnych globalnych na górze!!!
 
 #while (NB<MAX):
     #s_best = s
