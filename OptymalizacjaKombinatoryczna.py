62725bc80ac7b881b9c4b67492a5b17956e271d4
JoachimMakowski <joachim.makowski@student.put.poznan.pl>
2019-12-07 19:19:37 +0100

    Takie tam poprawki

--- OptymalizacjaKombinatoryczna.py
+++ OptymalizacjaKombinatoryczna.py
@@ -9,35 +9,9 @@ slownik={}  #wierzcholek:kolor
 n=0         #ilosc wierzcholkow
 lista_posortowanych=[]
 plik_z_krawedziami='graff0.3.txt'
+szansa_mutacji = 0.01
 
-def max_slownik(slowniki):      #zwraca ilosc kolorow
-    maxi=0
-    for x in slowniki.values():
-        if x>maxi:
-            maxi=x
-    return maxi
-
-def kolorowanie(macierz_polaczen,lista_krawedzi):       #funkcja kolorujaca zachlannie i zwracajaca slownik {indeks_wierzcholka:numer_koloru}
-    lista_wychodzacych=[]   #lista z wierzcholkami polaczonymi z jednym wierzcholkiem do którego dobieramy kolor
-    slownik_kolorow={}
-    for i in range(1,len(lista_krawedzi)+1):
-        slownik_kolorow[i]=0
-    kolor=0                 #numery oznaczają kolory
-    for i in lista_krawedzi:
-        for [x,y] in macierz_polaczen:
-            if x==i:
-                lista_wychodzacych.append(y)
-            elif y==i:
-                lista_wychodzacych.append(x)
-        for li in lista_wychodzacych:
-            if slownik_kolorow[li]==kolor:
-                kolor+=1
-        slownik_kolorow[i]=kolor
-        lista_wychodzacych=[]
-        kolor=1
-    return slownik_kolorow
-
-def losowanie_liczb(n):                         #zwraca liste z losowa kolejnoscia wierzcholkow
+def losowanie_listy_wierzcholkow(n):                         #zwraca liste z losowa kolejnoscia wierzcholkow
     lista_losowa=[i for i in range(1,n+1)]
     permutacja=list(permutations(lista_losowa))
     a=random.randint(0,len(permutacja))
@@ -61,11 +35,8 @@ for [x,y] in txt:
     tablica.append([x,y])
 
 lista_1_do_n=[i for i in range(1,n+1)]
-slownik = kolorowanie(tablica,lista_1_do_n)
-print("Ilość kolorow:",max_slownik(slownik))
 
 #zachlanny ulepszony
-print(40*"-")
 stopnie_wierzcholkow = {}
 for x in range(1,n+1):
     stopnie_wierzcholkow[x]=0
@@ -78,31 +49,27 @@ posortowane_wierzcholki = sorted(stopnie_wierzcholkow.items(), key=lambda x: x[1
 
 for x in posortowane_wierzcholki:
     lista_posortowanych.append(x[0])
-slownik_2=kolorowanie(tablica,lista_posortowanych)
-
-liczba_kolorow = max_slownik(slownik_2)
-print("Ilosc kolorow: ",liczba_kolorow)
-
 
 #zachlanny ulepszony, użycie Graf.py
 print(40*"-")
-graf2 = grafy.Graf(tablica,lista_1_do_n)
-graf1 = grafy.Graf(tablica,lista_posortowanych)
-print(graf1.slownik_wierzcholkow)
-print("Ilosc kolorow: ",graf1.max_z_slownika())
-print("wszystkie slowniki: ",grafy.Graf.lista_grafow)
+graf2 = grafy.Graf(macierz = tablica,lista_wierzcholkow = lista_1_do_n)
+graf1 = grafy.Graf(macierz = tablica,lista_wierzcholkow = lista_posortowanych)
+print("Ilosc kolorow: ",grafy.max_slownik(graf2.slownik_kolorow))
+'''print("wszystkie slowniki: ",grafy.Graf.lista_grafow)
 grafy.Graf.sortowanie_populacji()
 print("wszystkie slowniki: ",grafy.Graf.lista_grafow)
 grafy.Graf.odrzucanie_populacji()
-print("wszystkie slowniki: ",grafy.Graf.lista_grafow)
-
+print("wszystkie slowniki: ",grafy.Graf.lista_grafow)'''
+grafy.Graf.krzyzowanie(graf1,graf2)
+for x in grafy.Graf.lista_grafow:
+    print(x)
+    print(40*"-")
 
 M = [[0 for i in range(n)] for j in range(n)] #tabu matrix
-slownik_LF=kolorowanie(tablica,lista_posortowanych)
 
 NB = 0
-s = slownik_2 #initial confugiration generated with a greedy algorithm
-NC = liczba_kolorow - 1
+s = graf1.slownik_kolorow #initial confugiration generated with a greedy algorithm
+NC = grafy.max_slownik(grafy.Graf.lista_grafow[0]) - 1      #wczesniej trzeba posortowac lista_grafow
 MAX  = 10 #limit of the iterations
 
 #deklarowanie zmiennnych globalnych na górze!!!
--- module_graph.py
+++ module_graph.py
@@ -11,13 +11,13 @@ class Graf:         #klasa graf przechowuje pokolorowane wierzcholki grafu
 
     lista_grafow = []           #lista przechowuje wszystkie pokolorowania (slowniki) w populacji (pole statyczne)
 
-    def kolorowanie(self):       #funkcja kolorujaca zachlannie i zwracajaca slownik {indeks_wierzcholka:numer_koloru}
+    def kolorowanie(self,lista_wierzcholkow):       #funkcja kolorujaca zachlannie i zwracajaca slownik {indeks_wierzcholka:numer_koloru}
         lista_wychodzacych=[]   #lista z wierzcholkami polaczonymi z jednym wierzcholkiem do którego dobieramy kolor
         slownik_kolorow={}
-        for i in range(1,len(self.lista_krawedzi)+1):
+        for i in range(1,len(lista_wierzcholkow)+1):
             slownik_kolorow[i]=0
         kolor=0                 #numery oznaczają kolory
-        for i in self.lista_krawedzi:
+        for i in lista_wierzcholkow:
             for [x,y] in self.macierz:
                 if x==i:
                     lista_wychodzacych.append(y)
@@ -31,18 +31,24 @@ class Graf:         #klasa graf przechowuje pokolorowane wierzcholki grafu
             kolor=1
         return slownik_kolorow
     
-    def __init__(self,macierz,lista_krawedzi):      #inicjalizacja grafu
-        self.macierz = macierz
-        self.lista_krawedzi = lista_krawedzi
-        self.slownik_wierzcholkow = self.kolorowanie()      #kolorowanie grafu
-        Graf.lista_grafow.append(self.slownik_wierzcholkow) #dodawanie slownikow do listy
+    def __init__(self,macierz,lista_wierzcholkow = None, slownik_kolorow = None):      #inicjalizacja grafu
+        if(slownik_kolorow == None):
+            self.macierz = macierz
+            self.ilosc_bledow = 0
+            self.slownik_kolorow = self.kolorowanie(lista_wierzcholkow)      #kolorowanie grafu, slownik {wierzcholek:kolor}
+            Graf.lista_grafow.append(self.slownik_kolorow) #dodawanie slownikow do listy
+        elif(lista_wierzcholkow == None):
+            self.macierz = macierz
+            self.slownik_kolorow = slownik_kolorow
+            self.ilosc_bledow = self.szukanie_bledow
+            Graf.lista_grafow.append(self.slownik_kolorow)
 
-    def max_z_slownika(self):      #zwraca ilosc kolorow
-        maxi=0
-        for x in self.slownik_wierzcholkow.values():
-            if x>maxi:
-                maxi=x
-        return maxi
+    def szukanie_bledow(self):
+        licznik = 0
+        for [x,y] in self.macierz:
+            if (self.slownik_kolorow[x] == self.slownik_kolorow[y]):
+                licznik+=1
+        return licznik
 
     @staticmethod
     def odrzucanie_populacji():
@@ -51,3 +57,14 @@ class Graf:         #klasa graf przechowuje pokolorowane wierzcholki grafu
     @staticmethod 
     def sortowanie_populacji():
         Graf.lista_grafow.sort(key=max_slownik)
+
+    @staticmethod
+    def krzyzowanie(graf1,graf2):
+        slownik= {}
+        for x in range(1,len(graf1.slownik_kolorow)+1):
+            if(x<(len((graf1.slownik_kolorow))//2)+1):
+                slownik[x] = graf1.slownik_kolorow[x]
+            else:
+                slownik[x] = graf2.slownik_kolorow[x]
+        nowy_graf = Graf(macierz = graf1.macierz,slownik_kolorow=slownik)
+        nowy_graf.ilosc_bledow()
